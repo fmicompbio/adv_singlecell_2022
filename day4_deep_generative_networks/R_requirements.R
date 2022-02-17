0ee18bf32e76f261dd57790fc883523f3f6305ca
mbstadler <stadler.michael@gmail.com>
2022-02-17 13:41:35 +0100

    add folders for individual parts with empty requirement files

---
+++ day4_deep_generative_networks/R_requirements.R
@@ -0,0 +1,27 @@
+# R script to install requirements for exercises -------------------------------
+
+## global variables (edit in this section) -------------------------------------
+pkgs <- c()
+
+
+## install Bioconductor --------------------------------------------------------
+biocversion <- "3.14"
+if (!require("BiocManager", quietly = TRUE)) {
+    install.packages("BiocManager")
+}
+
+if (!identical(as.character(BiocManager::version()), biocversion)) {
+    BiocManager::install(version = biocversion)
+}
+
+
+## install additional packages -------------------------------------------------
+BiocManager::install(pkgs, update = FALSE)
+
+
+## check package loading -------------------------------------------------------
+for (pkg in pkgs)
+    library(pkg, character.only = TRUE)
+
+
+sessionInfo()
