2816060216d9a1a903ae8b47c8bc96243ce798e1
giovp <giov.pll@gmail.com>
2022-04-18 17:36:18 +0200

    update tutorials

--- day3_spatial_transcriptomics/save_img.py
+++ day3_spatial_transcriptomics/save_img.py
@@ -5,14 +5,14 @@ import imageio
 from skimage.util import img_as_ubyte
 
 LIB_ID = "V10F24-105_A1"
-root = pathlib.Path("/Users/giovanni.palla/Datasets/advomics")
-st_adata = sc.read(root / f"spatial_adata/{LIB_ID}.h5ad")
+path = pathlib.Path("/Users/giovanni.palla/Datasets/advomics/advomics_data")
+st_adata = sc.read(path / f"{LIB_ID}.h5ad"")
 
-img_path = root / f"{LIB_ID}.png"
+img_path = path / f"{LIB_ID}.png"
 logg.warning(f"Saved image at: {img_path}")
 imageio.imwrite(
     img_path,
-    img_as_ubyte(st_adata.uns["spatial"]["V10F24-105_A1"]["images"]["hires"]),
+    img_as_ubyte(st_adata.uns["spatial"][LIB_ID]["images"]["hires"]),
 )
-scalef = st_adata.uns["spatial"]["V10F24-105_A1"]["scalefactors"]["tissue_hires_scalef"]
+scalef = st_adata.uns["spatial"][LIB_ID]["scalefactors"]["tissue_hires_scalef"]
 logg.warning(f"Scalefactor is: {scalef}")
