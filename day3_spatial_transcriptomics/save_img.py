dc65a70b35c1442d99014238e4259c0e605a0268
giovp <giov.pll@gmail.com>
2022-04-18 17:40:31 +0200

    update path

--- day3_spatial_transcriptomics/save_img.py
+++ day3_spatial_transcriptomics/save_img.py
@@ -5,7 +5,7 @@ import imageio
 from skimage.util import img_as_ubyte
 
 LIB_ID = "V10F24-105_A1"
-path = pathlib.Path("/Users/giovanni.palla/Datasets/advomics/advomics_data")
+path = pathlib.Path("/Users/giovanni.palla/Datasets/advomics/advomics_data_spatial")
 st_adata = sc.read(path / f"{LIB_ID}.h5ad"")
 
 img_path = path / f"{LIB_ID}.png"
