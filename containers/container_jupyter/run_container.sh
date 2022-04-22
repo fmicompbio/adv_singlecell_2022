66b149796cfd6cd17e91184cdc8f667fff19529b
Geert van Geest <geert.vangeest@bioinformatics.unibe.ch>
2022-04-22 11:01:51 +0200

    adjustments to run local jupyter container

--- containers/container_jupyter/run_container.sh
+++ containers/container_jupyter/run_container.sh
@@ -1,10 +1,7 @@
 docker run \
 --user root \
--e NB_USER=juser \
 -e NB_UID=1000 \
 -e NB_UMASK=002 \
--w /home/juser \
--e CHOWN_HOME=yes \
 -e JUPYTER_ENABLE_LAB=yes \
 -e JUPYTER_TOKEN=test123 \
 -p 8888:8888 \
